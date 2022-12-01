dcbe9a15131369cb606e4f28f877ec626eff1acd
Leopoldo88 <114876690+Leopoldo88@users.noreply.github.com>
2022-12-01 11:56:39 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
+int n=0;
+cin>>n;
+if(is_prime(n))
+cout<<"numero primo"<<endl;
+else
+cout<<"numero non primo"<<endl;
+
+
+
   return 0;
 }
