f8747e62166ef5fca4bd40cfbe953af59f3ab025
Leopoldo88 <114876690+Leopoldo88@users.noreply.github.com>
2022-12-01 11:49:28 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+bool is_prime(int n)
+{
+if(n<=1) 
+return false;
+for(int i=2;i<n;i++)
+{
+if(n%i==0)
+return false;
+
+}
+return true;
+ }
+//per verificare n sia primo bisogna dividerlo per tutti i precedenti per definirlo ricorsivamente 
+
 
