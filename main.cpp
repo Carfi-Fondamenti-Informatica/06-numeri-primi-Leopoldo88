8ea72e5160c95bde45b4b2d6a0b85b4d1c919114
Leopoldo88 <114876690+Leopoldo88@users.noreply.github.com>
2022-12-02 20:43:23 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include "lib.h"
 using namespace std;
-int main(){
-int n=0;
-cin>>n;
-if(is_prime(n))
-cout<<"numero primo"<<endl;
-else
-cout<<"numero non primo"<<endl;
-return 0;
+
+int main (){{
+int a;
+bool b;
+  cin>>a;
+  b=primi(a);
+  
+  if(b==true)
+    cout<<"numero primo"<<endl;
+  else if(b==false)
+    cout<<"numero non primo"<<enl;
+  return 0;
 }
